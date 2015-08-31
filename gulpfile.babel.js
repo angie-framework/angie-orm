9568b5a85e8942bf471c0cc5136848961e9f5496
jgrosecl49 <joe.groseclose@ordergroove.com>
2015-08-30 20:55:57 -0400

    Fixed gulp task run

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,25 +2,26 @@ import {default as register} from   'babel-core/register';
 register({
     only: [
         '**/node_modules/angie*/**',
-        '**/src/**'
+        '**/src/**',
+        '**/test/**'
     ],
     stage: 0
 });
 
 // System Modules
-import fs from                      'fs';
-import gulp from                    'gulp';
-import {argv} from                  'yargs';
-import {exec} from                  'child_process';
-import eslint from                  'gulp-eslint';
-import jscs from                    'gulp-jscs';
-import istanbul from                'gulp-istanbul';
-import {Instrumenter} from          'isparta';
-import mocha from                   'gulp-mocha';
-import cobertura from               'istanbul-cobertura-badger';
-import esdoc from                   'gulp-esdoc';
-import babel from                   'gulp-babel';
-import {bold, red} from             'chalk';
+import fs from                              'fs';
+import gulp from                            'gulp';
+import {argv} from                          'yargs';
+import {exec} from                          'child_process';
+import eslint from                          'gulp-eslint';
+import jscs from                            'gulp-jscs';
+import {Instrumenter} from         'isparta';
+import mocha from                           'gulp-mocha';
+import istanbul from                        'gulp-istanbul';
+import cobertura from                       'istanbul-cobertura-badger';
+import esdoc from                           'gulp-esdoc';
+import babel from                           'gulp-babel';
+import {bold, red} from                     'chalk';
 
 const bread = (str) => bold(red(str));
 
@@ -48,40 +49,28 @@ gulp.task('jscs', [ 'eslint' ], function () {
             esnext: true
         }));
 });
-gulp.task('mocha', function(cb) {
-    let proc;
-
-    new Promise(function(resolve) {
-        proc = gulp.src(SRC).pipe(
-            istanbul({
-                instrumenter: Instrumenter,
-                includeUntested: true
-            })
-        ).pipe(
-            istanbul.hookRequire()
-        ).on('finish', function() {
-            gulp.src(
-                [ 'test/src/testUtil.spec.js', 'test/**/!(*testUtil).spec.js' ],
-                { read: false }
-            ).pipe(mocha({
-                reporter: 'spec'
-            }).on('error', function(e) {
-                throw new Error(e);
-            }).on('end', function() {
-                resolve();
-            }));
-        });
-    }).then(function() {
-        return proc.pipe(
-
-            // TODO fail if under accepted limit
-            istanbul.writeReports({
-                reporters: [ 'text', 'text-summary', 'cobertura', 'clover' ]
-            })
-        );
-    }).then(function() {
-        return cobertura('coverage/cobertura-coverage.xml', 'svg', cb);
-    });
+gulp.task('istanbul', function(cb) {
+    gulp.src('src/**/*.js').pipe(istanbul({
+        instrumenter: Instrumenter,
+        includeUntested: true,
+        babel: {
+            stage: 0
+        }
+    })).pipe(istanbul.hookRequire()).on('finish', cb);
+});
+gulp.task('mocha', [ 'istanbul' ], function() {
+    return gulp.src([
+        'test/src/testUtil.spec.js',
+        'test/**/*.spec.js'
+    ]).pipe(mocha({
+        reporter: 'spec'
+    })).pipe(istanbul.writeReports({
+        dir: 'coverage',
+        reportOpts: {
+            dir: 'coverage'
+        },
+        reporters: [ 'text', 'text-summary', 'json', 'html' ]
+    }));
 });
 gulp.task('babel', function() {
     return gulp.src('src/**').pipe(babel({
