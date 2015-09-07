93846f090812ef2f6845c9e71960c7cd5fdffa56
benderTheCrime <joseph.groseclose@spongecell.com>
2015-09-07 17:55:03 -0400

    Added README fixes

--- src/index.js
+++ src/index.js
@@ -62,7 +62,7 @@ function runTests() {
 
 function help() {
     $LogProvider.bold('Angie ORM');
-    console.log('A flexible, Promise-based ORM for the Angie MVC');
+    console.log('An extremely lightweight logging utility for NodeJS');
     console.log('\r');
     $LogProvider.bold('Version:');
     console.log(global.ANGIE_ORM_VERSION);
